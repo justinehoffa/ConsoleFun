da6bb3ca4e4b6ac521e77146b274654a2453bf5f
Justine Hoffa <justinefarrer@yahoo.com>
2020-08-24 08:38:47 -0600

    Add Convert Example

--- ConsoleFun.vb
+++ ConsoleFun.vb
@@ -2,10 +2,24 @@
 
     Sub Main()
         Dim userInput As String
-        Console.WriteLine("Please Type Some Text Then Press Enter...")
+        'Console.WriteLine("Please Type Some Text Then Press Enter...")
+        'userInput = Console.ReadLine()
+        'Console.WriteLine("Thanks!! You entered: " & userInput & ". Good Work!!")
+        'Console.ReadLine()
+
+        Dim firstNumber As Integer
+        Dim result As Integer
+        Console.WriteLine("Please Enter a Number...")
         userInput = Console.ReadLine()
-        Console.WriteLine("Thanks!! You entered: " & userInput & ". Good Work!!")
+        firstNumber = CUInt(userInput)
+        'firstNumber = Console.ReadLine()
+        result = 5 + firstNumber
+        Console.WriteLine(result)
         Console.ReadLine()
+
+
+
+
 
 
 
